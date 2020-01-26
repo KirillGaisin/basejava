de2e68d679788c78b7bc1b5cc5d7bb5ee8a78e7b
Kirill_Gaysin1 <kirill_gaysin1@epam.com>
2020-01-26 15:09:50 +0300

    Second commit. Updated several methods in ArrayStorage class: reworked getAll method in compliance with task requirements (no null entries), reworked delete method with compliance with task requirements (storage defragmentation), refactored fori cycles where they could be substituted with foreach

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,9 +7,9 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10];
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
+        for(Resume r: storage) {
             try {
-                storage[i].setUuid(null);
+                r.setUuid(null);
             } catch (NullPointerException ex) {
                 System.out.println("----------------------------\n" +
                         "Resume storage has been successfully cleared.");
@@ -17,10 +19,9 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        // без уникальности
         for (int i = 0; i < storage.length; i++) {
             try {
-                if(storage[i].getUuid().equals(r.getUuid())) {
+                if (storage[i].getUuid().equals(r.getUuid())) {
                     System.out.println("----------------------------\n" +
                             "Resume with this uuid is already in the storage! Enter another command");
                     return;
@@ -34,10 +35,10 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         // метод выбивается из общего принципа логгирования при поимке нпе из-за необходимости вернуть значение; не нравится дважды return null
-        for(int i=0; i<storage.length; i++) {
+        for (Resume r:storage) {
             try {
-                if(storage[i].getUuid().equals(uuid))
-                    return storage[i];
+                if (r.getUuid().equals(uuid))
+                    return r;
             } catch (NullPointerException ex) {
                 return null;
             }
@@ -46,13 +47,21 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        for(int i=0; i<storage.length; i++) {
+        for (int i = 0; i < storage.length; i++) {
             try {
-                if(storage[i].getUuid().equals(uuid))
+                if (storage[i].getUuid().equals(uuid)) {
                     storage[i].setUuid(null);
+                    // из-за этого цикла падаем в блок кетч, поэтому нет сообщения об ошибке при удалении несуществующего элемента (костыльно)
+                    for (int j = i + 1; j < storage.length; j++) {
+                        if (storage[j] != null) {
+                            storage[i] = storage[j];
+                            storage[j] = null;
+                            i++;
+                        }
+                    }
+                    break;
+                }
             } catch (NullPointerException ex) {
-                System.out.println("----------------------------\n" +
-                        "Element deleted. Enter another command");
                 return;
             }
         }
@@ -62,7 +71,15 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return storage;
+        Resume[] noNulls = new Resume[10];
+        // почему при вызове метода delete на последнем не нулевом элементе массива null остается в массиве в этом методе?
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                noNulls = Arrays.copyOf(storage, i);
+                break;
+            }
+        }
+        return noNulls;
     }
 
     int size() {
