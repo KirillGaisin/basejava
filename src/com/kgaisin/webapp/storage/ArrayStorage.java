4e15ec6233c84db45b2477955f491cfb181d6ae9
Kirill_Gaysin1 <kirill_gaysin1@epam.com>
2020-01-29 16:04:52 +0300

    partly got rid of code duping by moving cycling through resume storage into a standalone method

--- src/com/kgaisin/webapp/storage/ArrayStorage.java
+++ src/com/kgaisin/webapp/storage/ArrayStorage.java
@@ -9,34 +9,21 @@ import java.util.Arrays;
  */
 public class ArrayStorage {
 
-    /*
-    HW2
-    - Сделать проверки в update/get/delete - резюме есть в storage? DONE(?)
-    - В save - резюме нет в storage?
-    - В save проверку на переполнение DONE
-    - Избавиться от дублирования кода
-    - Методы из Arrays в clear и getAll DONE
-     */
-
     private Resume[] storage = new Resume[10];
 
     private int size = 0;
 
     public void clear() {
-        if (size == 0)
-            return;
-        Arrays.fill(storage, 0, size - 1, null);
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public void save(Resume resume) {
         //not null-safe
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(resume.getUuid())) {
-                System.out.println("----------------------------\n" +
-                        "Resume with uuid " + resume.getUuid() + " is already in the storage! Enter another command");
-                return;
-            }
+        if (checkForResumePresence(resume) >= 0) {
+            System.out.println("----------------------------\n" +
+                    "Resume with uuid " + resume.getUuid() + " is already in the storage! Enter another command");
+            return;
         }
 
         //проверка на заполненность storage
@@ -49,40 +36,38 @@ public class ArrayStorage {
         size++;
 
         //проверка на наличие сохраненного резюме в storage
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(resume.getUuid()))
-                return;
+        if(checkForResumePresence(resume) >= 0) {
+            return;
         }
         System.out.println("Resume with uuid " + resume.getUuid() + " was not added to the storage! Enter another command");
     }
 
     public void update(Resume resume) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(resume.getUuid())) {
-                System.out.println("----------------------------\n" +
-                        "Resume with uuid " + resume.getUuid() + " updated.");
-                return;
-            }
+        if (checkForResumePresence(resume) >= 0) {
+            System.out.println("----------------------------\n" +
+                    "Resume with uuid " + resume.getUuid() + " updated.");
+            return;
         }
         System.out.println("----------------------------\n" +
                 "Resume with uuid " + resume.getUuid() + " not found.");
     }
 
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid))
-                return storage[i];
+        Resume resumeToSearch = new Resume(uuid);
+        if (checkForResumePresence(resumeToSearch) >= 0) {
+            return resumeToSearch;
         }
         return null;
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                System.arraycopy(storage, i + 1, storage, i, size - i - 1);
-                size--;
-                return;
-            }
+        Resume resumeToDelete = new Resume(uuid);
+        if(checkForResumePresence(resumeToDelete) >= 0) {
+            int elementToDelete = checkForResumePresence(resumeToDelete);
+            storage[elementToDelete] = storage[size-1];
+            storage[size-1] = null;
+            size--;
+            return;
         }
         System.out.println("----------------------------\n" +
                 "Resume with uuid " + uuid + " not found.");
@@ -92,14 +77,19 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     public Resume[] getAll() {
-        Resume[] resumes = new Resume[size];
-        for (int i = 0; i < size; i++) {
-            resumes = Arrays.copyOf(storage, size);
-        }
-        return resumes;
+        return Arrays.copyOf(storage, size);
     }
 
     public int size() {
         return size;
+    }
+
+    private int checkForResumePresence(Resume resume) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(resume.getUuid())) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
